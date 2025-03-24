2bb644e63c0d05c9920a1045dc3b8bfe4e2df4b1
Александр Мышкин <sir.myshkin@gmail.com>
2025-03-24 11:43:27 +0300

    add gets metrics

--- metrics_py/main.py
+++ metrics_py/main.py
@@ -1,7 +1,10 @@
 import os
+import time
 from dotenv import load_dotenv
 import requests
 from requests.auth import HTTPBasicAuth
+from url_normalize import url_normalize
+from prometheus_client import start_http_server, Gauge
 
 load_dotenv()
 
@@ -9,45 +12,48 @@ NEXUS_API_URL = "http://sanich.space/service/rest/v1/repositories"
 NEXUS_USERNAME = os.getenv("NEXUS_USERNAME")
 NEXUS_PASSWORD = os.getenv("NEXUS_PASSWORD")
 
+# Метрика для хранения статусов репозиториев
+REPO_STATUS = Gauge("nexus_repo_status", "HTTP status of Nexus repositories", ["repo_name", "repo_type"])
 
 def check_repository_status(url):
+    """Проверяет статус репозитория по URL."""
     try:
         response = requests.get(url)
-        if response.status_code == 200:
-            return "Работает"
-        else:
-            return f"Ошибка {response.status_code}"
-    except requests.exceptions.RequestException as e:
-        return f"Ошибка запроса: {e}"
-
-
-def show_repo(repositories):
-    print(f"Получено {len(repositories)} репозиториев:")
-    for repo in repositories:
-        repo_name = repo["name"]
-        repo_url = repo["url"] + "/"
-        status = check_repository_status(repo_url)
-        print(f"Репозиторий: {repo_name} | URL: {repo_url} | Статус: {status}")
-
+        return response.status_code
+    except requests.exceptions.RequestException:
+        return 0  # 0 означает ошибку соединения
 
 def fetch_repositories():
+    """Получает список репозиториев и обновляет метрики."""
     try:
         response = requests.get(
             NEXUS_API_URL, auth=HTTPBasicAuth(NEXUS_USERNAME, NEXUS_PASSWORD)
         )
         response.raise_for_status()
-
         repositories = response.json()
-        print(repositories)
-        show_repo(repositories)
+
+        for repo in repositories:
+            normal_url = url_normalize(repo.get("url"))
+            status_code = check_repository_status(normal_url + "/")
+
+            repo_name = repo.get("name", "unknown")
+            repo_type = repo.get("type", "unknown")
+
+            # Обновляем метрику
+            REPO_STATUS.labels(repo_name=repo_name, repo_type=repo_type).set(status_code)
 
     except requests.exceptions.RequestException as e:
         print(f"Ошибка при получении репозиториев: {e}")
 
-
 def main():
-    fetch_repositories()
+    # Запускаем HTTP-сервер для метрик на порту 8000
+    start_http_server(8000)
+    print("Prometheus метрики доступны на http://localhost:8000")
 
+    # Бесконечный цикл для обновления метрик
+    while True:
+        fetch_repositories()
+        time.sleep(30)  # Обновляем метрики каждые 30 секунд
 
 if __name__ == "__main__":
     main()
