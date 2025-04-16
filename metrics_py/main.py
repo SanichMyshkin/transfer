9a5598ceba7931d65f3b17a8717f0dd74fd4e523
Александр Мышкин <sir.myshkin@gmail.com>
2025-04-16 11:36:11 +0300

    some fix

--- metrics_py/main.py
+++ metrics_py/main.py
@@ -5,8 +5,6 @@ from datetime import datetime
 from prometheus_client import start_http_server
 from dotenv import load_dotenv
 from proxyrepo_status import fetch_repositories_metrics
-
-# from hostedrepo_status import fetch_static_status
 from repo_size import fetch_repository_sizes
 from blobs_size import fetch_blob_metrics
 
@@ -26,17 +24,12 @@ load_dotenv()
 NEXUS_API_URL = os.getenv("NEXUS_API_URL")
 NEXUS_USERNAME = os.getenv("NEXUS_USERNAME")
 NEXUS_PASSWORD = os.getenv("NEXUS_PASSWORD")
-TIME_RUN_STORAGE = os.getenv("TIME_RUN_STORAGE")
 DB_URL = os.getenv("DATABASE_URL")
 
 
 def get_auth():
     """Возвращает кортеж с логином и паролем для Nexus"""
     return (NEXUS_USERNAME, NEXUS_PASSWORD)
-
-
-def should_run_blob_metrics():
-    return datetime.now().strftime("%H:%M") == TIME_RUN_STORAGE
 
 
 def main():
@@ -46,17 +39,13 @@ def main():
 
     while True:
         logging.info("Запуск сбора статуса репозиториев типа Proxy...")
-
         fetch_repositories_metrics(NEXUS_API_URL, auth)
-        # logging.info("Запуск сбора статуса репозиториев типа Hosted и Group...")
-        # fetch_static_status(NEXUS_API_URL, auth)
-
-        # if should_run_blob_metrics():
+        
         logging.info("Запуск сбора размера репозиториев и блобов...")
         fetch_blob_metrics(NEXUS_API_URL, auth)
         fetch_repository_sizes(NEXUS_API_URL, DB_URL, auth)
 
-        time.sleep(30)
+        time.sleep(900)
 
 
 if __name__ == "__main__":
